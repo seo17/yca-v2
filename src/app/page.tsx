11792cdd7d1c30e5e82fa84dc98ca484c58d45a8
ERIOLUWA AKANDE <73904970+CYBERIC-EA@users.noreply.github.com>
2024-09-26 04:21:56 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Features from "@/components/Features";
 import FinalCTA from "@/components/FinalCTA";
 import Footer from "@/components/Footer";
@@ -6,6 +7,16 @@ import Navbar from "@/components/Navbar";
 import Pricing from "@/components/Pricing";
 
 export default function Home() {
+  useEffect(() => {
+    // Create the meta tag
+    const meta = document.createElement("meta");
+    meta.name = "google-adsense-account";
+    meta.content = "ca-pub-4668077427204289"; // Replace with your AdSense account
+    
+    // Add it to the document head
+    document.head.appendChild(meta);
+  }, []);
+
   return (
     <>
       <Navbar />
